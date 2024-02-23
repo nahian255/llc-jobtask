2183b3de611346d785365d80849ae9a0c596c16b
nahian255 <nahimn253@gmail.com>
2024-02-24 00:42:51 +0600

    nav added

---
+++ src/navbar/Nab.jsx
@@ -0,0 +1,10 @@
+
+const Nab = () => {
+    return (
+        <div className="h-16 bg-black text-white">
+            <h1 className="text-white">nav</h1>
+        </div>
+    );
+};
+
+export default Nab;
