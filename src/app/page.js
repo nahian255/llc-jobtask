73c448e80c7971702db325b2dd6cb31cd1aebc25
nahian255 <nahimn253@gmail.com>
2024-02-24 20:26:45 +0600

    navbar link added

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import Nab from "@/navbar/Nab";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="">
       {/* Navbar  */}
       {/* <Nab></Nab> */}
-      <div className="h-8 bg-black text-white">
-        <h1 className="text-white">nav</h1>
+      <div className="flex justify-evenly h-12 text-xl items-center bg-black text-white">
+        <Link href={'/'}> Categories</Link>
+        <Link href={'/'}> Website Builders</Link>
+        <Link href={'/'}> Today&apos;s deals</Link>
       </div>
       <h1 className="text-4xl">Best Website builders in the US</h1>
     </main>
